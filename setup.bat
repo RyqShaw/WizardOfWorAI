0b0382e3244caff1b17b87895b15c41cf9fa6e77
Sharyq Siddiqi <62477104+RyqShaw@users.noreply.github.com>
2025-05-28 18:29:49 -0400

    Setup Scripts

--- setup.bat
+++ setup.bat
@@ -1,4 +1,4 @@
 python -m venv wor_ai_env
 call wor_ai_env\Scripts\activate
 pip install -r requirements.txt
-echo Setup complete. Run with: venv\Scripts\activate && python main.py
+echo Setup complete. Run with: wor_ai_env\Scripts\activate && python main.py
--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 python3 -m venv wor_ai_env
 source wor_ai_env/bin/activate
 pip install -r requirements.txt
-echo "Setup complete. Run with: source venv/bin/activate && python main.py"
+echo "Setup complete. Run with: source wor_ai_env/bin/activate && python main.py"
