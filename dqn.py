adda70f9f174b0367b8475ee707dab23038b5ba5
Sharyq Siddiqi <62477104+RyqShaw@users.noreply.github.com>
2025-06-10 10:11:15 -0400

    Added Layer to DQN; Made basic forward implementation

--- dqn.py
+++ dqn.py
@@ -6,10 +6,12 @@ class DQN(nn.Module):
   def __init__(self, input_dimension, output_dimension):
     super.__init__()
     self.stack = nn.Sequential(
-      nn.Linear(input_dimension, 128),
+      nn.Linear(input_dimension, 128), # Observation layer
       nn.ReLU(),
-      # start with 128 (smaller side)
-      nn.Linear(128, output_dimension)
-      
+      nn.Linear(128, 128), # Hidden Layer
+      nn.ReLU(),
+      nn.Linear(128, output_dimension) # Action Layer
     )
     
+  def forward(self, x):
+    return self.stack(x)
