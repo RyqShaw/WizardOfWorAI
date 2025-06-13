f9475a3375837c99cd5642ebb72d39b3a230c607
Sharyq Siddiqi <62477104+RyqShaw@users.noreply.github.com>
2025-06-13 15:54:06 -0400

    Sharyq's Personal Branch Test

--- training.py
+++ training.py
@@ -8,22 +8,29 @@ from dqn import DQN
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from ale_py import ALEInterface
+from torch.amp import autocast, GradScaler
 
 # Setup environment
-rendering = "rgb_array"
 gym.register_envs(ale_py)
-env = gym.make("ALE/WizardOfWor-v5", render_mode=rendering, obs_type="rgb")
+env = gym.make("ALE/WizardOfWor-v5", obs_type="grayscale")
 
-device = "cpu"
-if torch.cuda.is_available():
-    device = "cuda"
-elif torch.mps.is_available():
-    device = "mps"
-        
+env = gym.wrappers.ResizeObservation(env, (84, 84))
+env = gym.wrappers.FrameStackObservation(env, 4)
+
+ale = env.unwrapped.ale
+
 # Get env info
 obs, info = env.reset()
-next_obs, reward, terminated, truncated, info = env.step(env.action_space.sample())
-print(next_obs.shape, reward, terminated, truncated, info)
+
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+
 state_size = obs.shape[0]
 action_size = env.action_space.n
 
@@ -32,38 +39,47 @@ checkpoint_interval = 50
 model_path = "nn.pth"
 
 # Training: does 64 concurrent episodes by default, uses DQN and Replay Buffer Impl
-def train(batch_size=64, gamma=0.999, epsilon=1, decay=.999, max_episodes=100, load_checkpoint = False):
+def train(batch_size=64, gamma=0.999, epsilon=1, decay=.999, max_episodes=100, min_epsilon=0.05, max_episode_steps=18000, load_checkpoint = False):
     # Save Episodes
     current_epsilon = epsilon
     episode_rewards = []
     replay_buffer = ReplayBuffer(10000)
 
     # DQN and Torch Setup
-    dqn = DQN(state_size, action_size, device).to(device)
+    policy_nn = DQN(state_size, env.action_space.n).to(device)
+    target_nn = DQN(state_size, env.action_space.n).to(device)
+    min_replay_size = 5000
     #print("Initialized DQN", dqn.parameters())
     mse = torch.nn.MSELoss()
-    optimizer = torch.optim.Adam(dqn.parameters(), lr=0.001)
+    optimizer = torch.optim.Adam(dqn.parameters(), lr=0.00025)
     #print("Initialized Optimizer")
     
     #Load Checkpoint if one exists
-    if os.path.exists("checkpoint.pth") and load_checkpoint:
+    episodes_done = 0
+    total_steps = 0
+    #Load Checkpoint if needed
+    if os.path.exists("checkpoint.path") and load_checkpoint:
         print("Loading Checkpoint")
-        checkpoint = torch.load("checkpoint.pth")
-        dqn.load_state_dict(checkpoint['model_state_dict'])
+        checkpoint = torch.load("checkpoint.path")
+        episodes_done = checkpoint['episode']
+        policy_nn.load_state_dict(checkpoint['policy_state_dict'])
+        target_nn.load_state_dict(checkpoint['target_state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
         epsilon = checkpoint['epsilon']
-        episode_rewards = checkpoint['episode_rewards']
     
     # Training Loop
     print("Starting Training:")
-    for episode in range(max_episodes):
+    for episode in range(episodes_done, max_episodes):
         observation, info = env.reset()
         episode_over = False
         total_reward = 0
+        episode_steps = 0
+        current_lives = ale.lives()
 
         if episode % print_statement_interval == 0:
             print(f"Episode {episode} / {max_episodes}")
         while not episode_over:
+            normalized_obs = obs.astype(np.float32) / 255.0
             # Epsilon Greedy: random or optimal from DQN
             action = 0
             if random.random() < current_epsilon:
@@ -74,65 +90,107 @@ def train(batch_size=64, gamma=0.999, epsilon=1, decay=.999, max_episodes=100, l
                     q_values = dqn.forward(state_tensor)
                     action = torch.argmax(q_values).item()
 
-            # print(action)
-            next_obs, reward, terminated, truncated, info = env.step(action)
-            if action == 0:
-                reward -= 0.01
-            # reward = np.clip(reward, -1, 1)
-            # if reward > 0:
-
-            #     print(reward)
-            next_obs = np.array(next_obs).flatten()
-
+            new_obs, reward, terminated, truncated, info = env.step(action)
+            total_steps += 1
+            episode_steps += 1
+            done = terminated or truncated
+            
+            if ale.lives() < current_lives:
+                reward -= 10.0
+                current_lives = ale.lives()
+            else:
+                reward -= 0.001
+            
+            # Scales reward
+            clipped_reward = np.clip(reward, -10, 10)
+            
+            # add to buffer
+            new_obs_array = np.array(new_obs)
+            normalized_new_obs = new_obs_array.astype(np.float32) / 255.0
+           
             # Store experience
             replay_buffer.add(observation, action, reward, next_obs)
+            
+            next_obs = np.array(next_obs).flatten()
+            
             observation = next_obs
             total_reward += reward
 
             # Train DQN, Storing SARS
-            if len(replay_buffer) >= batch_size:
+            if len(replay_buffer) >= min_replay_size and total_steps % 4 == 0:
+                scaler = GradScaler()
+                
                 states, actions, rewards, next_states = replay_buffer.sample(batch_size)
                 states = torch.FloatTensor(states).to(device)
                 actions = torch.LongTensor(actions).unsqueeze(1).to(device)
                 rewards = torch.FloatTensor(rewards).unsqueeze(1).to(device)
                 next_states = torch.FloatTensor(next_states).to(device)
 
-                q_values = dqn.forward(states).gather(1, actions)
-                with torch.no_grad():
-                    next_q_values = dqn(next_states).max(1, keepdim=True)[0]
-                    target_q = rewards + gamma * next_q_values
-                loss = mse(q_values, target_q)
-                # print(loss)
-                # if info['frame_number'] > 3080:
-                #     return
+                with autocast(device):
+                    q_vals = policy_nn.forward(states).gather(1, actions.unsqueeze(1))
+                    
+                    with torch.no_grad():
+                        # Q Learning, Getting the max from the first state and making that the target_q
+                        next_actions = policy_nn(next_states).argmax(1)
+                        next_q_vals = target_nn(next_states).gather(1, next_actions.unsqueeze(1)).squeeze()
+                        target_q_vals = rewards + (gamma * next_q_vals)
 
+                    # Loss Calculations
+                    loss = mse(q_vals.squeeze(), target_q_vals)
+                
+                # Optimizer to update gradients
+                scaler.scale(loss).backward()
+                scaler.unscale_(optimizer)
+                
+                # Gradient clipping to prevent instability in training
+                torch.nn.utils.clip_grad_norm_(policy_nn.parameters(), max_norm=10.0)
+                
+                # Free some memory now
+                del states, next_states, q_vals, target_q, loss
+                
+                scaler.step(optimizer)
+                scaler.update()
                 optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
+                
+                # Free some memory now
+                torch.cuda.empty_cache()
+                
+                # Update target every 1000 steps AI takes
+                if total_steps % 1000 == 0:
+                    target_nn.load_state_dict(policy_nn.state_dict())
 
             epsilon = max(0.1, epsilon * decay)
             
             episode_over = truncated or terminated
+             # Episode Limit
+            if episode_steps >= max_episode_steps:
+                episode_over = True
         episode_rewards.append(total_reward)
         #print(total_reward)
         
-        # Save Periodically incase of crash every 50 episodes:
-        if episode % checkpoint_interval == 0 and episode != 0:
-            print(f"Checkpoint Reached: {episode}")
+            
+
+        epsilon = max(min_epsilon, epsilon * decay)
+        
+        # Update Target NN + checkpoint
+        if episode % 100 == 0 and episode != 0:
+            print("Checkpoint!")
             checkpoint = {
                 'episode': episode,
-                'model_state_dict': dqn.state_dict(),
+                'policy_state_dict': policy_nn.state_dict(),
+                'target_state_dict': target_nn.state_dict(),
                 'optimizer_state_dict': optimizer.state_dict(),
                 'epsilon': epsilon,
-                'episode_rewards': episode_rewards
             }
-            torch.save(checkpoint, "checkpoint.pth")
-    plt.scatter(range(max_episodes), episode_rewards)
-    plt.show()
+            torch.save(checkpoint, "checkpoint.path")
+    #plt.scatter(range(max_episodes), episode_rewards)
+    #plt.show()
             
     return dqn
+
 start_time = time.time()
 dqn = train(max_episodes=100, load_checkpoint = False)
+
 #run this on sharyq gpu when confident it all works
 #dqn = train(batch_size=256, max_episodes=15000, load_checkpoint = False)
 print(f"Training time: {time.time() - start_time}")
